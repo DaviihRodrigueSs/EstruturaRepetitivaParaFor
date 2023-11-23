ea42c026c0778f98302c6e4ed36050a2b5d97105
daviu <daviu@DESKTOP-0BEA9Q2>
2023-11-23 19:43:33 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+namespace EstruturaRepetitivaFor
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            // For geralmente e usado quando você sabe a quantidade de repetições diferente da estrutura repetitiva While.
+
+            int N, x, soma;
+            N = int.Parse(Console.ReadLine());
+
+            soma = 0;
+            // inicio; condição; incremento)
+            for (int i = 1; i <= N; i = i + 1) // ou i++ que seria a mesma coisa
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+
+
+            }
+            Console.WriteLine(soma);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
